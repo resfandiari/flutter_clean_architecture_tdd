728371f024efd85094ec2f0f2a194d7674207ae8
resfandiari <resfandiari.dev@gmail.com>
2020-04-25 22:32:05 +0430

    Data Layer Overview & Models

--- test/features/number_trivia/domain/usecases/get_random_number_trivia_test.dart
+++ test/features/number_trivia/domain/usecases/get_random_number_trivia_test.dart
@@ -2,7 +2,7 @@ import 'package:dartz/dartz.dart';
 import 'package:flutter_clean_architecture_tdd/core/usecases/usecase.dart';
 import 'package:flutter_clean_architecture_tdd/features/number_trivia/domain/entities/number_trivia.dart';
 import 'package:flutter_clean_architecture_tdd/features/number_trivia/domain/repositories/number_trivia_repository.dart';
-import 'package:flutter_clean_architecture_tdd/features/number_trivia/domain/usecases/GetRandomNumberTrivia.dart';
+import 'package:flutter_clean_architecture_tdd/features/number_trivia/domain/usecases/get_random_number_trivia.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 
