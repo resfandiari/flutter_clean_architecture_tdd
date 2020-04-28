3ad38b4a09a50bbc9be9f120e57558fd180afd33
resfandiari <resfandiari.dev@gmail.com>
2020-04-28 23:37:01 +0430

    Network Info

--- lib/features/number_trivia/data/repositories/number_trivia_repository_impl.dart
+++ lib/features/number_trivia/data/repositories/number_trivia_repository_impl.dart
@@ -1,7 +1,7 @@
 import 'package:dartz/dartz.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_clean_architecture_tdd/core/error/failures.dart';
-import 'package:flutter_clean_architecture_tdd/core/platform/network_info.dart';
+import 'package:flutter_clean_architecture_tdd/core/network/network_info.dart';
 import 'package:flutter_clean_architecture_tdd/features/number_trivia/data/datasources/number_trivia_local_data_source.dart';
 import 'package:flutter_clean_architecture_tdd/features/number_trivia/data/datasources/number_trivia_remote_data_source.dart';
 import 'package:flutter_clean_architecture_tdd/features/number_trivia/domain/entities/number_trivia.dart';
--- test/features/number_trivia/data/repositories/number_trivia_repository_impl_test.dart
+++ test/features/number_trivia/data/repositories/number_trivia_repository_impl_test.dart
@@ -1,7 +1,7 @@
 import 'package:dartz/dartz.dart';
 import 'package:flutter_clean_architecture_tdd/core/error/exception.dart';
 import 'package:flutter_clean_architecture_tdd/core/error/failures.dart';
-import 'package:flutter_clean_architecture_tdd/core/platform/network_info.dart';
+import 'package:flutter_clean_architecture_tdd/core/network/network_info.dart';
 import 'package:flutter_clean_architecture_tdd/features/number_trivia/data/datasources/number_trivia_local_data_source.dart';
 import 'package:flutter_clean_architecture_tdd/features/number_trivia/data/datasources/number_trivia_remote_data_source.dart';
 import 'package:flutter_clean_architecture_tdd/features/number_trivia/data/models/number_trivia_model.dart';
