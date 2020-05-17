199a12d3f7129dd7f18ea629d821707800dc6caf
resfandiari <resfandiari.dev@gmail.com>
2020-05-17 22:09:09 +0430

    Dependency Injection

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+import 'injection_container.dart' as di;
+
+void main() async {
+  await di.init();
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
