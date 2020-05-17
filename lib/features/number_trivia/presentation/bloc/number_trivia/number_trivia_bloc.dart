0b93c06f3da757151a90c6feb73fc20b075c39a9
resfandiari <resfandiari.dev@gmail.com>
2020-05-17 17:46:42 +0430

    Bloc Implementation 2/2

--- lib/features/number_trivia/presentation/bloc/number_trivia/number_trivia_bloc.dart
+++ lib/features/number_trivia/presentation/bloc/number_trivia/number_trivia_bloc.dart
@@ -1,7 +1,11 @@
 import 'dart:async';
 
 import 'package:bloc/bloc.dart';
+import 'package:dartz/dartz.dart';
+import 'package:flutter_clean_architecture_tdd/core/error/failures.dart';
+import 'package:flutter_clean_architecture_tdd/core/usecases/usecase.dart';
 import 'package:flutter_clean_architecture_tdd/core/util/input_converter.dart';
+import 'package:flutter_clean_architecture_tdd/features/number_trivia/domain/entities/number_trivia.dart';
 import 'package:flutter_clean_architecture_tdd/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart';
 import 'package:flutter_clean_architecture_tdd/features/number_trivia/domain/usecases/get_random_number_trivia.dart';
 import 'package:flutter_clean_architecture_tdd/features/number_trivia/presentation/bloc/number_trivia/number_trivia_event.dart';
@@ -19,12 +23,9 @@ class NumberTriviaBloc extends Bloc<NumberTriviaEvent, NumberTriviaState> {
   final InputConverter inputConverter;
 
   NumberTriviaBloc({
-    // Changed the name of the constructor parameter (cannot use 'this.')
     @required GetConcreteNumberTrivia concrete,
     @required GetRandomNumberTrivia random,
     @required this.inputConverter,
-    // Asserts are how you can make sure that a passed in argument is not null.
-    // We omit this elsewhere for the sake of brevity.
   })  : assert(concrete != null),
         assert(random != null),
         assert(inputConverter != null),
@@ -46,8 +47,37 @@ class NumberTriviaBloc extends Bloc<NumberTriviaEvent, NumberTriviaState> {
         (failure) async* {
           yield Error(message: INVALID_INPUT_FAILURE_MESSAGE);
         },
-        (integer) => throw UnimplementedError(),
+        (integer) async* {
+          yield Loading();
+          final failureOrTrivia =
+              await getConcreteNumberTrivia(Params(number: integer));
+          yield* _eitherLoadedOrErrorState(failureOrTrivia);
+        },
       );
+    } else if (event is GetTriviaForRandomNumber) {
+      yield Loading();
+      final failureOrTrivia = await getRandomNumberTrivia(NoParams());
+      yield* _eitherLoadedOrErrorState(failureOrTrivia);
+    }
+  }
+
+  Stream<NumberTriviaState> _eitherLoadedOrErrorState(
+    Either<Failure, NumberTrivia> failureOrTrivia,
+  ) async* {
+    yield failureOrTrivia.fold(
+      (failure) => Error(message: _mapFailureToMessage(failure)),
+      (trivia) => Loaded(trivia: trivia),
+    );
+  }
+
+  String _mapFailureToMessage(Failure failure) {
+    switch (failure.runtimeType) {
+      case ServerFailure:
+        return SERVER_FAILURE_MESSAGE;
+      case CacheFailure:
+        return CACHE_FAILURE_MESSAGE;
+      default:
+        return 'Unexpected error';
     }
   }
 }
