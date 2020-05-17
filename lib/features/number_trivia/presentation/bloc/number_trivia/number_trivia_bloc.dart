bed0dc4b456d0899fc0e57739c535c0006aac76a
resfandiari <resfandiari.dev@gmail.com>
2020-05-17 17:18:08 +0430

    Bloc Implementation 1/2

--- lib/features/number_trivia/presentation/bloc/number_trivia/number_trivia_bloc.dart
+++ lib/features/number_trivia/presentation/bloc/number_trivia/number_trivia_bloc.dart
@@ -1,17 +1,53 @@
 import 'dart:async';
+
 import 'package:bloc/bloc.dart';
+import 'package:flutter_clean_architecture_tdd/core/util/input_converter.dart';
+import 'package:flutter_clean_architecture_tdd/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart';
+import 'package:flutter_clean_architecture_tdd/features/number_trivia/domain/usecases/get_random_number_trivia.dart';
+import 'package:flutter_clean_architecture_tdd/features/number_trivia/presentation/bloc/number_trivia/number_trivia_event.dart';
+import 'package:flutter_clean_architecture_tdd/features/number_trivia/presentation/bloc/number_trivia/number_trivia_state.dart';
 import 'package:meta/meta.dart';
 
-part 'number_trivia_event.dart';
-
-part 'number_trivia_state.dart';
+const String SERVER_FAILURE_MESSAGE = 'Server Failure';
+const String CACHE_FAILURE_MESSAGE = 'Cache Failure';
+const String INVALID_INPUT_FAILURE_MESSAGE =
+    'Invalid Input - The number must be a positive integer or zero.';
 
 class NumberTriviaBloc extends Bloc<NumberTriviaEvent, NumberTriviaState> {
-  @override
-  NumberTriviaState get initialState => InitialNumberTriviaState();
+  final GetConcreteNumberTrivia getConcreteNumberTrivia;
+  final GetRandomNumberTrivia getRandomNumberTrivia;
+  final InputConverter inputConverter;
+
+  NumberTriviaBloc({
+    // Changed the name of the constructor parameter (cannot use 'this.')
+    @required GetConcreteNumberTrivia concrete,
+    @required GetRandomNumberTrivia random,
+    @required this.inputConverter,
+    // Asserts are how you can make sure that a passed in argument is not null.
+    // We omit this elsewhere for the sake of brevity.
+  })  : assert(concrete != null),
+        assert(random != null),
+        assert(inputConverter != null),
+        getConcreteNumberTrivia = concrete,
+        getRandomNumberTrivia = random;
 
   @override
-  Stream<NumberTriviaState> mapEventToState(NumberTriviaEvent event) async* {
-    // TODO: Add your event logic
+  NumberTriviaState get initialState => Empty();
+
+  @override
+  Stream<NumberTriviaState> mapEventToState(
+    NumberTriviaEvent event,
+  ) async* {
+    if (event is GetTriviaForConcreteNumber) {
+      final inputEither =
+          inputConverter.stringToUnsignedInteger(event.numberString);
+
+      yield* inputEither.fold(
+        (failure) async* {
+          yield Error(message: INVALID_INPUT_FAILURE_MESSAGE);
+        },
+        (integer) => throw UnimplementedError(),
+      );
+    }
   }
 }
